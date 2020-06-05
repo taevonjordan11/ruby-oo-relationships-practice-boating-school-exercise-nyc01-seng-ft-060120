99abf2de96cc2dae4de50e4e2edb77a53a2504c7
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-06-05 13:39:38 -0400

    worked on student class

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,36 @@
 class Student
+@@all = []
 
+  attr_reader :first_name
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(name, status, instructor)
+    BoatingTest.new(self, name, status, instructor)
+  end
+
+  def self.find_student(first_name)
+    @@all.find{|student| student.first_name == first_name}
+  end
+
+
+  def grade_percentage
+    test = BoatingTest.all.select do |tests|
+      tests.student == self
+    end
+
+    passed = test.select do |tests|
+      tests.status == "passed"
+    end
+    (passed.count/test.count).to_f
+  end
+
+  
 end
